c08c8cec867e49bf0e9fb309794ac2f5024a5c11
Henri Tuan <henri.tuan77@gmail.com>
2024-10-12 12:27:50 +0200

    chore: format documents + page structure

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="grid items-center justify-items-center p-8 pb-20 gap-16">
+      <main className="">Home</main>
+    </div>
+  );
+}
